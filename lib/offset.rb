bb30daec68c295cdc46235af043272f1cebcbd45
antciccone <aciccone@bu.edu>
2016-08-30 08:20:43 -0600

    Additions to Encipher and Decrypting

---
+++ lib/offset.rb
@@ -0,0 +1,9 @@
+class Offset
+
+  def date_number
+  date = Time.now.strftime("%d%m%y").to_i
+  date = date ** 2
+  date.to_s[-4..-1]
+
+  end
+end
---
+++ test/key_test.rb
@@ -0,0 +1,16 @@
+require "./lib/key"
+require 'minitest/autorun'
+require 'minitest/pride'
+
+
+class KeyTest < Minitest::Test
+  def test_class_is_created
+    a = Key.new
+    assert_equal Key, a.class
+  end
+
+  def test_generate_a_random_key_lengh_of_5
+    a = Key.new
+    assert_equal 5,  a.random_key.length
+  end
+end
